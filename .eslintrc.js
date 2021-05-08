a01de5ec960aa4a63fe47a3590d9b73d28d2c93a
Humberto Mendes <humbertoarm@gmail.com>
2021-05-07 22:17:56 -0500

    * Updated eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': 0,
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'import/prefer-default-export': 0,
